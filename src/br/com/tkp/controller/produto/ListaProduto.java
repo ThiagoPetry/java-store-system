207ab85993beeb3865fd39af4c05af9851770cf5
petry <thiagokeiserpetry@gmail.com>
2021-05-11 16:51:43 -0300

    Listagem de dados do banco

--- src/br/com/tkp/controller/produto/ListaProduto.java
+++ src/br/com/tkp/controller/produto/ListaProduto.java
@@ -1,19 +1,42 @@
 package br.com.tkp.controller.produto;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-import br.com.tkp.model.ProdutoModel;
+import br.com.dao.DataBaseConnection;
 
 public class ListaProduto {
-	public List<ProdutoModel> listarProdutos(List<ProdutoModel> produtos) {
-		System.out.println("\n============ Produtos Cadastrados =============");
-		System.out.printf("| %2s | %8s | %8s | %4s | %9s |\n", "ID", "Produto", "Preço", "Qtd", "R$ total");
 
-		produtos.forEach(produto -> {
-			System.out.printf("| %2s | %8s | %8s | %4s | %9s |\n", produtos.indexOf(produto), produto.getNomeProd(),
-					produto.getPrecoProd(), produto.getQtdProd(), produto.getSaldoEstoque());
-		});
+	private Connection connection;
 
-		return produtos;
+	public ListaProduto() {
+		connection = DataBaseConnection.getInstance().getConnection();
+	}
+
+	public ResultSet listarProdutos() {
+		PreparedStatement preparedStatement;
+
+		try {
+			String sql = "SELECT * FROM produto";
+			preparedStatement = connection.prepareStatement(sql);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			
+			System.out.println("\n============== Produtos Cadastrados ==============");
+			System.out.printf("| %2s | %13s | %6s | %4s | %9s |\n", "ID", "Produto", "Preço", "Qtd", "R$ total");
+			System.out.println("==================================================");
+			
+			while(resultSet.next()) {
+				System.out.printf("| %2s | %13s | %6s | %4s | %9s |\n", 
+						resultSet.getInt("codigoDoProduto"), 
+						resultSet.getString("nomeDoProduto"),
+						resultSet.getDouble("precoDoProduto"), 
+						resultSet.getInt("quantidadeDoProduto"),
+						resultSet.getDouble("saldoEmEstoque"));
+			}
+			return resultSet;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 }
