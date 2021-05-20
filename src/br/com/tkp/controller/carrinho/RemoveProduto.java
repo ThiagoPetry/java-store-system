122d88e02a635b1de202b4c7ecb9b40382e2949c
petry <thiagokeiserpetry@gmail.com>
2021-05-20 10:45:18 -0300

    Retirada de alguns métodos em troca de código menor

--- src/br/com/tkp/controller/carrinho/RemoveProduto.java
+++ src/br/com/tkp/controller/carrinho/RemoveProduto.java
@@ -2,133 +2,53 @@ package br.com.tkp.controller.carrinho;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Scanner;
 
 import br.com.dao.DataBaseConnection;
 
 public class RemoveProduto {
-	
+
 	private Scanner tec = new Scanner(System.in);
+	private VerificaSeExisteProduto verificaSeExisteProduto;
 	private Connection connection;
 
 	public RemoveProduto() {
 		connection = DataBaseConnection.getInstance().getConnection();
 	}
-	
-	public boolean verificaSeExisteProduto(int idDoProduto) {
-		PreparedStatement preparedStatement;
-		try {
-			String sql = "SELECT * FROM carrinho WHERE codigoDoProduto = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, idDoProduto);
-			
-			ResultSet resultSet = preparedStatement.executeQuery();
-			
-			if(!resultSet.next()) {
-				System.out.println("Este produto não existe no carrinho.");
-				return false;
-			} else {
-				return true;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public double procuraPrecoDoProduto(String cliente, int id) {
-		PreparedStatement preparedStatement;
-		ResultSet resultSet;
-		double recebePreco;
-		
-		try {
-			String sql = "SELECT * FROM produto WHERE codigoDoProduto = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
-			
-			resultSet.next();
-			recebePreco = resultSet.getDouble("precoDoProduto");			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return -1;
-		}
-		return recebePreco;
-	}
-	
-	public int procuraQuantidadeDoProduto(String cliente, int id) {
-		PreparedStatement preparedStatement;
-		ResultSet resultSet;
-		int recebeQtd;
-		
-		try {
-			String sql = "SELECT * FROM produto WHERE codigoDoProduto = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
-			
-			resultSet.next();
-			recebeQtd = resultSet.getInt("quantidadeDoProduto");			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return -1;
-		}
-		return recebeQtd;
-	}
-	
-	public int procuraQuantidadeDoProdutoCar(String cliente, int id) {
-		PreparedStatement preparedStatement;
-		ResultSet resultSet;
-		int recebeQtd;
-		
-		try {
-			String sql = "SELECT * FROM carrinho WHERE codigoDoProduto = ?";
-			preparedStatement = connection.prepareStatement(sql);
-			preparedStatement.setInt(1, id);
-			resultSet = preparedStatement.executeQuery();
-			
-			resultSet.next();
-			recebeQtd = resultSet.getInt("quantidadeDoProduto");			
-		} catch (Exception e) {
-			e.printStackTrace();
-			return -1;
-		}
-		return recebeQtd;
-	}
-	
-	public void removerProdutoCarrinho(String cliente) {
+
+	public void removerProdutoCarrinho(int cliente) {
 		PreparedStatement preparedStatementDel, preparedStatementAtu;
-	  //PreparedStatement preparedStatementAtu, preparedStatementPro, preparedStatementCar;
+		verificaSeExisteProduto = new VerificaSeExisteProduto();
+		int idDoProduto;
 
 		System.out.println("\n===== Remover produto do carrinho =====");
-		System.out.print("\nInforme o ID do produto a ser removido: ");
-		int idDoProduto = tec.nextInt();
-		
-		try {			
-			if(!verificaSeExisteProduto(idDoProduto)) {
+		System.out.print("Informe o ID do produto a ser removido: ");
+		idDoProduto = tec.nextInt();
+
+		try {
+			if (verificaSeExisteProduto.procurarPeloProduto(idDoProduto) == false) {
 				return;
 			}
-		
-			String sql_delete = "DELETE FROM carrinho WHERE codigoDoProduto = ? AND nomeDoUsuario = ?";
-			preparedStatementDel = connection.prepareStatement(sql_delete);
-			preparedStatementDel.setInt(1, idDoProduto);
-			preparedStatementDel.setString(2, cliente);
-			preparedStatementDel.execute();
-			
-//			procuraPrecoDoProduto(cliente, idDoProduto);
-//			procuraQuantidadeDoProduto(cliente, idDoProduto);
-//          ========================================================
-			
-			int qtd = procuraQuantidadeDoProduto(cliente, idDoProduto) + procuraQuantidadeDoProdutoCar(cliente, idDoProduto);
-			
-			String sql_atualiza = "UPDATE produto SET quantidadeDoProduto = ?, saldoEmEstoque = ? WHERE codigoDoProduto = ?";
+
+			// Atualiza a tabela produto
+			String sql_atualiza = "UPDATE produto SET quantidadeDoProduto = quantidadeDoProduto + "
+					+ "(SELECT quantidadeDoProduto FROM carrinho WHERE codigoDoProduto = ? AND codigoDoUsuario = ?), "
+					+ "saldoEmEstoque = quantidadeDoProduto * precoDoProduto WHERE codigoDoProduto = ?;";
+
 			preparedStatementAtu = connection.prepareStatement(sql_atualiza);
-			preparedStatementAtu.setInt(1, qtd);
-			preparedStatementAtu.setDouble(2, (procuraPrecoDoProduto(cliente, idDoProduto) * qtd));
+			preparedStatementAtu.setInt(1, idDoProduto);
+			preparedStatementAtu.setInt(2, cliente);
 			preparedStatementAtu.setInt(3, idDoProduto);
 			preparedStatementAtu.execute();
-			
+
+			// Remove o produto do carrinho
+			String sql_delete = "DELETE FROM carrinho WHERE codigoDoProduto = ? AND codigoDoUsuario = ?";
+
+			preparedStatementDel = connection.prepareStatement(sql_delete);
+			preparedStatementDel.setInt(1, idDoProduto);
+			preparedStatementDel.setInt(2, cliente);
+			preparedStatementDel.execute();
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println("Não foi possível excluir essa informação.");
